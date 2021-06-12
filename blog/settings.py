af5eb2487fea3f6bb8e6cb09d99cfdb39097b0de
Vitaliy <vzaharkiv28@gmail.com>
2021-06-12 15:12:57 +0300

    Text

--- blog/settings.py
+++ blog/settings.py
@@ -73,13 +73,6 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': config('ENGINE'),
